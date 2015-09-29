052e08f2cf8803f8cfcda6089b225f5ae6000571
Jorge Téllez <novohispano@gmail.com>
2015-09-28 23:37:01 -0600

    Add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link_to(body, url, html_options = {})
+    if url == request.path
+      link_to(body, url, class: 'active')
+    else
+      link_to(body, url)
+    end
+  end
 end
