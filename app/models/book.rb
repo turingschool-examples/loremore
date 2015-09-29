3a3749752344d02bb9ec8398f1542aa9db71bcdd
Jorge Téllez <novohispano@gmail.com>
2015-09-29 00:19:53 -0600

    Add books and quotes views

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ActiveRecord::Base
+  def self.generate
+    5000.times do
+      Book.create(
+        title:     Faker::Book.title,
+        author:    Faker::Name.name,
+        publisher: Faker::Book.publisher
+      )
+    end
+  end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,11 @@
 class Quote < ActiveRecord::Base
+  def self.generate
+    5000.times do
+      Quote.create(
+        body:   Faker::Hacker.say_something_smart.capitalize,
+        author: Faker::Name.name,
+        title:  Faker::Name.title
+      )
+    end
+  end
 end
